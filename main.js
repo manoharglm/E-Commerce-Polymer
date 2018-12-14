80a29d466f782fd835f385e81351578914dd235e
manohar_gunduboina <manohar.gunduboina@mountblue.io>
2018-12-14 18:03:32 +0530

    Bugs fixed and working on cart page

---
+++ main.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("title-nav-bar-side").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("title-nav-bar-side").style.width = "0";
+}
+// document.getElementsByClassName("item-description-dropdown-size").onclick = function() {
+//         lineOpacity()
+// };
+// function lineOpacity(){
+//     document.getElementsByClassName("item-description-dropdown-size").style.border = "1px solid rgba(0,0,0,0.5)";
+// }
